31a2228fd301158c0b9fd125b7cc9d658773ac21
Merlijn Vos <merlijn.vos043@gmail.com>
2017-02-09 17:01:30 +0100

    clean code

--- src/SPA/static/js/app.js
+++ src/SPA/static/js/app.js
@@ -7,14 +7,16 @@
 
 	const sections = {
 		toggle: route => {
-			allSections.forEach(section => route === `#${section.id}` ? section.classList.remove('hide') : section.classList.add('hide'));
+			allSections.forEach(section => {
+				route === `#${section.id}` ? section.classList.remove('hide') : section.classList.add('hide');
+			});
 		}
 	};
 
 	const routes = {
 		init: () => {
 			window.location.hash ? sections.toggle(window.location.hash) : sections.toggle(`#${allSections[0].id}`);
-			window.onhashchange = () => sections.toggle(window.location.hash)
+			window.onhashchange = () => sections.toggle(window.location.hash);
 		}
 	};
 
