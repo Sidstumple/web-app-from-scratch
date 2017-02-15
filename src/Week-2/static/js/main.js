07d78ccbaae9f0e33e5bb1c6fa714c26a9691254
Merlijn Vos <merlijn.vos043@gmail.com>
2017-02-15 16:52:54 +0100

    Make everything DRY friendly

--- src/Week-2/static/js/main.js
+++ src/Week-2/static/js/main.js
@@ -4,25 +4,28 @@
 	'strict mode';
 
 	const controller = {
-		listen() {
-			const chosenPlanet = document.querySelector('.dropdown-planets');
-			chosenPlanet.addEventListener('change', () => {
-				if (chosenPlanet.value.length > 1) {
-					view.clean();
-					view.update(chosenPlanet.value);
-				}
+		listen(arr) {
+			const dropdowns = Array.from(document.querySelectorAll('select'));
+
+			dropdowns.forEach(dd => {
+				dd.addEventListener('change', () => {
+					if (dd.value.length > 1) {
+						view.clean();
+						view.update(arr, dd.value, dd.name);
+					}
+				});
 			});
 		}
 	};
 
 	const view = {
-		init(data) {
-			data.forEach(planet => {
-				const section = document.querySelector('.dropdown-planets');
+		init(arr, selector) {
+			arr.forEach(endpoint => {
+				const section = document.querySelector(selector);
 				section.insertAdjacentHTML('beforeend',
-					`<option value="${planet.name}">${planet.name}</option>`);
+					`<option value="${endpoint.name}">${endpoint.name}</option>`);
 			});
-			controller.listen();
+			controller.listen(arr);
 		},
 
 		clean() {
@@ -32,18 +35,44 @@
 			});
 		},
 
-		update(chosenPlanet) {
-			model.planets.forEach(planet => {
-				if (planet.name === chosenPlanet) {
+		update(arr, value, type) {
+			arr.forEach(item => {
+				if (item.name === value) {
 					const section = document.querySelector('section');
-					section.insertAdjacentHTML('beforeend',
-						`<p>Name: <span>${planet.name}</span></p>
-						<p>Diameter: <span>${planet.diameter}</span></p>
-						<p>Diameter: <span>${planet.gravity}</span></p>
-						<p>Diameter: <span>${planet.population}</span></p>
-						<p>Diameter: <span>${planet.climate}</span></p>
-						<p>Diameter: <span>${planet.terrain}</span></p>`
-					);
+					if (type === 'planets') {
+						console.log(item.name);
+						section.insertAdjacentHTML('beforeend',
+							`<p>Name: <span>${item.name}</span></p>
+							<p>Diameter: <span>${item.diameter}</span></p>
+							<p>Gravity: <span>${item.gravity}</span></p>
+							<p>Population: <span>${item.population}</span></p>
+							<p>Climate: <span>${item.climate}</span></p>
+							<p>Terrain: <span>${item.terrain}</span></p>`
+						);
+					}
+					if (type === 'people') {
+						console.log(item.name);
+						section.insertAdjacentHTML('beforeend',
+							`<p>Name: <span>${item.name}</span></p>
+							<p>Birth year: <span>${item.birth_year}</span></p>
+							<p>Eye color: <span>${item.eye_color}</span></p>
+							<p>Gender: <span>${item.gender}</span></p>
+							<p>Hair color: <span>${item.hair_color}</span></p>
+							<p>Height: <span>${item.height}</span></p>`
+						);
+					}
+					if (type === 'starships') {
+						console.log(item.name);
+						section.insertAdjacentHTML('beforeend',
+							`<p>Name: <span>${item.name}</span></p>
+							<p>Model: <span>${item.model}</span></p>
+							<p>Starship class: <span>${item.starship_class}</span></p>
+							<p>Manufacturer: <span>${item.manufacturer}</span></p>
+							<p>Cost in credits: <span>${item.cost_in_credits}</span></p>
+							<p>Length: <span>${item.length}</span></p>
+							<p>Crew: <span>${item.crew}</span></p>`
+						);
+					}
 				}
 			});
 		}
@@ -51,10 +80,13 @@
 
 	const model = {
 		init() {
-			this.request('planets', this.planets);
+			// (API endpoint, array with data from endpoint, dropdown selector)
+			this.request('planets', this.planets, '.dropdown-planets');
+			this.request('people', this.people, '.dropdown-people');
+			this.request('starships', this.starships, '.dropdown-starships');
 		},
 
-		async request(endpoint, arr) {
+		async request(endpoint, arr, selector) {
 			try {
 				let counter = 1;
 				let data;
@@ -64,7 +96,7 @@
 					arr.push(...data.results);
 					counter++;
 				} while (data.next);
-				view.init(arr);
+				view.init(arr, selector);
 			} catch (err) {
 				console.error(err);
 			}
