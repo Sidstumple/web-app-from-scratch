b073c0ca9781669863e539c70e85d15e3e0a95e2
Merlijn Vos <merlijn.vos043@gmail.com>
2017-02-15 15:11:05 +0100

    insertAdjacentHTML, handle multiple API requests

--- src/Week-2/static/js/main.js
+++ src/Week-2/static/js/main.js
@@ -18,10 +18,9 @@
 	const view = {
 		init(data) {
 			data.forEach(planet => {
-				const option = document.createElement('option');
-				option.setAttribute('value', planet.name);
-				option.appendChild(document.createTextNode(planet.name));
-				document.querySelector('.dropdown-planets').appendChild(option);
+				const section = document.querySelector('.dropdown-planets');
+				section.insertAdjacentHTML('beforeend',
+					`<option value="${planet.name}">${planet.name}</option>`);
 			});
 			controller.listen();
 		},
@@ -36,38 +35,43 @@
 		update(chosenPlanet) {
 			model.planets.forEach(planet => {
 				if (planet.name === chosenPlanet) {
-					const name = document.createElement('p');
-					name.appendChild(document.createTextNode(`Name: ${planet.name}`));
-					document.querySelector('section').appendChild(name);
-
-					const diameter = document.createElement('p');
-					diameter.appendChild(document.createTextNode(`Diameter: ${planet.diameter}`));
-					document.querySelector('section').appendChild(diameter);
-
-					const gravity = document.createElement('p');
-					gravity.appendChild(document.createTextNode(`Gravity: ${planet.gravity}`));
-					document.querySelector('section').appendChild(gravity);
-
-					const population = document.createElement('p');
-					population.appendChild(document.createTextNode(`Population: ${planet.population}`));
-					document.querySelector('section').appendChild(population);
+					const section = document.querySelector('section');
+					section.insertAdjacentHTML('beforeend',
+						`<p>Name: <span>${planet.name}</span></p>
+						<p>Diameter: <span>${planet.diameter}</span></p>
+						<p>Diameter: <span>${planet.gravity}</span></p>
+						<p>Diameter: <span>${planet.population}</span></p>
+						<p>Diameter: <span>${planet.climate}</span></p>
+						<p>Diameter: <span>${planet.terrain}</span></p>`
+					);
 				}
 			});
 		}
 	};
 
 	const model = {
-		async init() {
+		init() {
+			this.request('planets', this.planets);
+		},
+
+		async request(endpoint, arr) {
 			try {
-				const response = await fetch('http://swapi.co/api/planets/');
-				const data = await response.json();
-				this.planets.push(...data.results);
-				view.init(data.results);
+				let counter = 1;
+				let data;
+				do {
+					let response = await fetch(`http://swapi.co/api/${endpoint}/?page=${counter}`);
+					data = await response.json();
+					arr.push(...data.results);
+					counter++;
+				} while (data.next);
+				view.init(arr);
 			} catch (err) {
 				console.error(err);
 			}
 		},
-		planets: []
+		planets: [],
+		people: [],
+		starships: []
 	};
 
 	model.init();
