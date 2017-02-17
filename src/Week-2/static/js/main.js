e30b80fc0de7e9dbd1947d093e447c38b02f88d8
Merlijn Vos <merlijn.vos043@gmail.com>
2017-02-17 12:12:01 +0100

    Split update, fix multiple bug firing events multiple times

--- src/Week-2/static/js/main.js
+++ src/Week-2/static/js/main.js
@@ -4,32 +4,48 @@
 	'strict mode';
 
 	const controller = {
-		listen(arr) {
-			const dropdowns = Array.from(document.querySelectorAll('select'));
-			dropdowns.forEach(dd => {
-				dd.addEventListener('change', event => {
-					if (dd.value.length > 1) {
-						console.log(event);
-						// Instead of updating the DOM I remove and insert
-						// new content everytime because insertAdjacentHTML is
-						// 142 times faster than innerHTML
-						// Source: https://jsperf.com/insertadjacenthtml-perf/28
-						view.clean();
-						view.update(arr, dd.value, dd.name);
+		listen(endpoint, arr, selector) {
+			let dropdown = document.querySelector(selector);
+			dropdown.addEventListener('change', () => {
+				if (dropdown.value.length > 1) {
+					// Instead of updating the DOM I remove and insert
+					// new content everytime because insertAdjacentHTML is
+					// 142 times faster than innerHTML
+					// Source: https://jsperf.com/insertadjacenthtml-perf/28
+					view.clean();
+
+					if (endpoint === 'planets') {
+						view.planets(arr, dropdown.value);
 					}
-				});
+					if (endpoint === 'people') {
+						view.people(arr, dropdown.value);
+					}
+					if (endpoint === 'starships') {
+						view.starships(arr, dropdown.value);
+					}
+				}
 			});
 		}
 	};
 
 	const view = {
-		init(arr, selector) {
-			arr.forEach(endpoint => {
+		init(endpoint, arr, selector) {
+			arr.forEach(item => {
 				const section = document.querySelector(selector);
 				section.insertAdjacentHTML('beforeend',
-					`<option value="${endpoint.name}">${endpoint.name}</option>`);
+					`<option value="${item.name}">${item.name}</option>`);
 			});
-			controller.listen(arr);
+			controller.listen(endpoint, arr, selector);
+		},
+
+		crawl() {
+			// Don't judge. I have tried it all in CSS, seriously.
+			// It takes a while before the transform inits
+			// In the meanwhile the DOM is loaded and you see the entire crawl
+			// instead of a slide in.
+			setTimeout(function () {
+				document.querySelector('section').classList.remove('hide');
+			}, 2500);
 		},
 
 		clean() {
@@ -39,48 +55,60 @@
 			});
 		},
 
-		update(arr, value, type) {
+		planets(arr, value) {
+			const dropdowns = document.querySelectorAll('select');
+			const section = document.querySelector('section');
 			arr.forEach(item => {
 				if (item.name === value) {
-					const dropdowns = document.querySelectorAll('select');
-					const section = document.querySelector('section');
-					if (type === 'planets') {
-						dropdowns[1].selectedIndex = 0;
-						dropdowns[2].selectedIndex = 0;
-						section.insertAdjacentHTML('beforeend',
-							`<p>Name: <span>${item.name}</span></p>
-							<p>Diameter: <span>${item.diameter}</span></p>
-							<p>Gravity: <span>${item.gravity}</span></p>
-							<p>Population: <span>${item.population}</span></p>
-							<p>Climate: <span>${item.climate}</span></p>
-							<p>Terrain: <span>${item.terrain}</span></p>`
-						);
-					}
-					if (type === 'people') {
-						dropdowns[0].selectedIndex = 0;
-						dropdowns[2].selectedIndex = 0;
-						section.insertAdjacentHTML('beforeend',
-							`<p>Name: <span>${item.name}</span></p>
-							<p>Birth year: <span>${item.birth_year}</span></p>
-							<p>Eye color: <span>${item.eye_color}</span></p>
-							<p>Gender: <span>${item.gender}</span></p>
-							<p>Hair color: <span>${item.hair_color}</span></p>
-							<p>Height: <span>${item.height}</span></p>`
-						);
-					}
-					if (type === 'starships') {
-						dropdowns[0].selectedIndex = 0;
-						dropdowns[1].selectedIndex = 0;
-						section.insertAdjacentHTML('beforeend',
-							`<p>Name: <span>${item.name}</span></p>
-							<p>Model: <span>${item.model}</span></p>
-							<p>Starship class: <span>${item.starship_class}</span></p>
-							<p>Manufacturer: <span>${item.manufacturer}</span></p>
-							<p>Cost in credits: <span>${item.cost_in_credits}</span></p>
-							<p>Length: <span>${item.length}</span></p>
-							<p>Crew: <span>${item.crew}</span></p>`
-						);
-					}
+					dropdowns[1].selectedIndex = 0;
+					dropdowns[2].selectedIndex = 0;
+					section.insertAdjacentHTML('beforeend',
+						`<p>Name: <span>${item.name}</span></p>
+						<p>Diameter: <span>${item.diameter}</span></p>
+						<p>Gravity: <span>${item.gravity}</span></p>
+						<p>Population: <span>${item.population}</span></p>
+						<p>Climate: <span>${item.climate}</span></p>
+						<p>Terrain: <span>${item.terrain}</span></p>`
+					);
+				}
+			});
+		},
+
+		people(arr, value) {
+			const dropdowns = document.querySelectorAll('select');
+			const section = document.querySelector('section');
+			arr.forEach(item => {
+				if (item.name === value) {
+					dropdowns[0].selectedIndex = 0;
+					dropdowns[2].selectedIndex = 0;
+					section.insertAdjacentHTML('beforeend',
+						`<p>Name: <span>${item.name}</span></p>
+						<p>Birth year: <span>${item.birth_year}</span></p>
+						<p>Eye color: <span>${item.eye_color}</span></p>
+						<p>Gender: <span>${item.gender}</span></p>
+						<p>Hair color: <span>${item.hair_color}</span></p>
+						<p>Height: <span>${item.height}</span></p>`
+					);
+				}
+			});
+		},
+
+		starships(arr, value) {
+			const dropdowns = document.querySelectorAll('select');
+			const section = document.querySelector('section');
+			arr.forEach(item => {
+				if (item.name === value) {
+					dropdowns[0].selectedIndex = 0;
+					dropdowns[1].selectedIndex = 0;
+					section.insertAdjacentHTML('beforeend',
+						`<p>Name: <span>${item.name}</span></p>
+						<p>Model: <span>${item.model}</span></p>
+						<p>Starship class: <span>${item.starship_class}</span></p>
+						<p>Manufacturer: <span>${item.manufacturer}</span></p>
+						<p>Cost in credits: <span>${item.cost_in_credits}</span></p>
+						<p>Length: <span>${item.length}</span></p>
+						<p>Crew: <span>${item.crew}</span></p>`
+					);
 				}
 			});
 		}
@@ -88,6 +116,7 @@
 
 	const model = {
 		init() {
+			view.crawl();
 			// (API endpoint, array with data from endpoint, dropdown selector)
 			this.request('planets', this.planets, '.dropdown-planets');
 			this.request('people', this.people, '.dropdown-people');
@@ -107,7 +136,7 @@
 					arr.push(...data.results);
 					counter++;
 				} while (data.next); // Until next: null
-				view.init(arr, selector);
+				view.init(endpoint, arr, selector);
 			} catch (err) {
 				console.error(err);
 			}
